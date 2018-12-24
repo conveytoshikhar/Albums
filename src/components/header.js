0696f2497459695345161e4861dedfe39f017fdd
Shikhar Khandelwal <shikhark@Shikhars-MBP.domain.name>
2018-12-24 22:16:39 +0530

    Intital Commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//Import Libraries for component
+import React from 'react';
+import { Text } from 'react-native';
+
+//Make a component 
+const Header = () => (<Text>Albums</Text>);
+
+
+//Make component avaiable 
+export default Header;
+
