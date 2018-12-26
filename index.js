9312e95b98de69c3c9a430da11df815f9c282e32
Shikhar Khandelwal <shikhark@Shikhars-MBP.domain.name>
2018-12-26 17:46:46 +0530

    Axios added for http requests in ajax

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, AppRegistry } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
+
 //create a component
 const App = () => {
     return (
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List</Text>
-        </View>
-    );
-};
-
-
+class AlbumList extends Component {
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response.data[0]));
+    }
+    componentDidMount() {
+        console.log('Componet did mount');
+    }
+    render() {
+        return (
+            <View>
+                <Text>Album List!!</Text>
+            </View>
+        );
+    }
+}
 export default AlbumList;
 
