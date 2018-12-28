4354b00355d6306eda0387b48713710b5845e17a
Shikhar Khandelwal <shikhark@Shikhars-MBP.domain.name>
2018-12-28 13:49:08 +0530

    Album Detail in the making

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+const AlbumDetail = (props) => {
+    const album = props.album; 
+
+    
+}
+
+
+export default AlbumDetail;
+
