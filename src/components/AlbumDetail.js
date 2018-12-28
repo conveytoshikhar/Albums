53c296cc026a65de9b2624b5b8bede337503aef7
Shikhar Khandelwal <shikhark@Shikhars-MBP.domain.name>
2018-12-28 14:34:08 +0530

    More on styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-    const album = props.album; 
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { thumbNailStyle, headerContentStyle } = styles; //destructuring
     return (
         <Card>
-            <Text>{album.title}</Text>
+            <CardSection>
+                <View>
+                    <Image style={thumbNailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>  
+            </CardSection>
         </Card>
     );
 };
 
+const styles = {
+    headerContentStyle: {
+       flexDirection: 'column',
+       justifyContent: 'space-around' 
+    },
+    thumbNailStyle: {
+        height: 50,
+        width: 50,
+        marginRight: 10
+    }
+};
 
 export default AlbumDetail;
 
